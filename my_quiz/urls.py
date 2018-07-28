fc2ab5102069cda5ea6f8d6425e83ea62d793c42
unknown <ishiishun10@gmail.com>
2018-07-28 18:01:10 +0900

    expanding commit

--- my_quiz/urls.py
+++ my_quiz/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.appmain, name='appmain'),
+    url(r'^fifa$', views.appfifa, name='appfifa'),
+    url(r'^flag$', views.appflag, name='appflag'),
 ]
--- my_quiz/views.py
+++ my_quiz/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def appmain(request):
-   return render(request, 'quiz/fifa.html', {})
+   return render(request, 'quiz/main.html', {})
+
+def appfifa(request):
+    return render(request, 'quiz/fifa.html', {})
+
+def appflag(request):
+    return render(request, 'quiz/flag.html', {})
